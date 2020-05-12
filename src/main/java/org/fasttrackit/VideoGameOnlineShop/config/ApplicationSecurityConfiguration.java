1a3ebbdf0ab94b606354a811f61886054859df44
Neculai Robert <robert.neculai13@gmail.com>
2020-05-12 14:35:39 +0300

    Started working on Security via Spring Security

---
+++ src/main/java/org/fasttrackit/VideoGameOnlineShop/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,46 @@
+package org.fasttrackit.VideoGameOnlineShop.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ApplicationSecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/products")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails loloman=User.builder()
+                .username("loloman")
+                .password(passwordEncoder.encode("231952"))
+                .roles("admin")
+                .build();
+        return  new InMemoryUserDetailsManager(loloman);
+    }
+}
